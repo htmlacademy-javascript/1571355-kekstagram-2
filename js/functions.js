0b8dc21e526e436be04b193bf334e692bc0f14f4
Alexlmth <alex.kungs@gmail.com>
2026-04-23 22:41:00 +0300

    Д/З_2.33 Исправил ошибки

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,6 @@
 //Функция для проверки длины строки.
 const checkStringLength = (string = '', maxSimbols = 1) => string.length <= maxSimbols;
+window.checkStringLength = checkStringLength;
 
 //Функция для проверки, является ли строка палиндромом.
 
@@ -7,3 +8,4 @@ const isPalindrome = (string) => {
   const normalizedString = string.replaceAll(' ', '').toLowerCase();
   return normalizedString === normalizedString.split('').reverse().join('');
 };
+window.isPalindrome = isPalindrome;
